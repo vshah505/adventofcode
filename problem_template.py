c305c0d3ef3d915dcfd6f94d769eba8e96e3abf0
Vishal Shah <vishal@hioscar.com>
2022-12-02 00:19:57 -0500

    Add problem template

---
+++ problem_template.py
@@ -0,0 +1,19 @@
+import utils
+
+
+def get_input():
+    with utils.get_input_fd(__file__) as f:
+        pass
+
+
+def part1():
+    pass
+
+
+def part2():
+    pass
+
+
+if __name__ == "__main__":
+    print(f"Part 1: {part1()}")
+    print(f"Part 2: {part2()}")
